644631abb4e294ae155c6f1b4efe36f993e92be3
n_k00001 <nooorykhalef@gmail.com>
2024-05-19 00:27:01 +0300

    delete

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-TZDLDNXG.mjs";import"./chunk-BJJR5UGY.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-RGNWAOKM.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
